b411585f8d677993a404a6fdbe6251ef51cffc58
vishnukant Jadhav <vishnukantjadhav1996@gmail.com>
2023-06-01 15:43:26 +0530

    commited Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,8 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int a = 15 ;
+        int b = 25 ;
+
+        System.out.println("Multiplication of two numbers is: "+(b*a));
+    }
+}
