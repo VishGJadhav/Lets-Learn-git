2638b9e037de814723117b1376674c591bee2649
vishnukant Jadhav <vishnukantjadhav1996@gmail.com>
2023-06-01 15:28:56 +0530

    Adding Diff.java

---
+++ Diff.java
@@ -0,0 +1,8 @@
+public class Diff {
+    public static void main(String[] args) {
+        int a = 15 ;
+        int b = 25 ;
+
+        System.out.println("Difference of two numbers is: "+(b-a));
+    }
+}
